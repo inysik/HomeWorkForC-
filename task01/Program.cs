679be76e21b64344e832afcb3b30fa5eae34e701
InessaRyzhikova <inessa1992@yandex.ru>
2022-10-03 00:23:27 +0300

    Added homeWorks

---
+++ task01/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход
+// принимает два числа и выдаёт, какое число большее, а
+// какое меньшее.
+
+Console.Write(" Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write(" Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+
+// int a = 5; int b = 6;
+if (b > a)
+    Console.WriteLine($"{b} больше, а {a} меньше");
+else
+    Console.WriteLine($"{a} больше, а {b} меньше");
---
+++ task02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход три числа и выдаёт максимальное из этих чисел.
+
+
+Console.Write(" Введите число a = ");
+int a = int.Parse(Console.ReadLine());
+Console.Write(" Введите число b = ");
+int b = int.Parse(Console.ReadLine());
+Console.Write(" Введите число v = ");
+int v = int.Parse(Console.ReadLine());
+
+
+int x = Math.Max(a, Math.Max(b, v));
+
+Console.Write($"максимальное число {x}");
